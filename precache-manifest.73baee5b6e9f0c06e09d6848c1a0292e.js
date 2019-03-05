29125e88a5dc022abb03b4650230f0bff0c73d80
Kamil <kjstepaniak1@gmail.com>
2019-03-05 22:13:51 +0000

    Updates

---
+++ precache-manifest.73baee5b6e9f0c06e09d6848c1a0292e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9517119be8c13d6fd4",
+    "url": "/advanced-react-todo/static/js/runtime~main.8e951711.js"
+  },
+  {
+    "revision": "c5d39620356a257f15c7",
+    "url": "/advanced-react-todo/static/js/main.c5d39620.chunk.js"
+  },
+  {
+    "revision": "3341dd069a95620a29b5",
+    "url": "/advanced-react-todo/static/js/2.3341dd06.chunk.js"
+  },
+  {
+    "revision": "c5d39620356a257f15c7",
+    "url": "/advanced-react-todo/static/css/main.060b1a11.chunk.css"
+  },
+  {
+    "revision": "9ed41ae9c42e6371649fa974041380fe",
+    "url": "/advanced-react-todo/index.html"
+  }
+];
